32e3c1522f757518c3778008bddcc748f12ccf6c
hakimmudzaki <166127079+hakimmudzaki@users.noreply.github.com>
2024-05-12 21:01:14 +0700

    Update _00_LCG.py
    *style*

--- src/_00_LCG.py
+++ src/_00_LCG.py
@@ -1,31 +1,16 @@
-# KAMUS
-# seed : integer
-# ALGORITMA
-from time import time
-seed = int(time())
-# seed akan terus berubah
-# Mengembalikan angka random dengan algoritma Linear Congruential Generator (LCG) dengan angka rentang 0 - batas_atas.
-#seed: Nilai awal (biji) untuk memulai urutan.
-#a: Pengganda dalam rumus LCG.
-#c: Penambahan dalam rumus LCG.
-#m: Modulus dalam rumus LCG.
 
-#rumus LCG= (a*seed + b) mod m
-#syarat : 0<m , 0<a<m , 0<=x<m, 0<seed<m
-def LCG(batas_atas : int) -> int:
-    # KAMUS LOKAL:
-        #const a : integer = 1664525
-        # constb c : integer = 1013904223
-        # const m : integer = 2^32
-    
-    # ALGORITMA
+from time import time
+
+def LCG(upper_limit : int) -> int:
+    """
+    membuat angka random dengan rentang 1-upper_limit
+    """    
     a = 1664525
     c = 1013904223
     m = 2**32
-    global seed
+    seed = int(time())
     seed = (a*seed + c) % m
-    return seed % (batas_atas + 1)
+    return seed % (upper_limit + 1)
 
 if __name__ == '__main__':
-    x=LCG(100)
-    print(x)
+    x=LCG()
